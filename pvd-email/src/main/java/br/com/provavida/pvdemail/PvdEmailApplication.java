056ce246a86b60ade356ecd9b9122f485e130806
keillaarr <keillaarruda9@gmail.com>
2025-07-15 14:46:50 -0300

    primeiro commit

---
+++ pvd-email/src/main/java/br/com/provavida/pvdemail/PvdEmailApplication.java
@@ -0,0 +1,13 @@
+package br.com.provavida.pvdemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvdEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvdEmailApplication.class, args);
+	}
+
+}
---
+++ pvd-email/src/test/java/br/com/provavida/pvdemail/PvdEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.provavida.pvdemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvdEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
